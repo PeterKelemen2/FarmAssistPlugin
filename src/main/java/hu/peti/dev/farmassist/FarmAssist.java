22120fd83226ba213af33aea1efdd25fa583114c
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-22 15:28:09 +0100

    Fixed it a bit

--- src/main/java/hu/peti/dev/farmassist/FarmAssist.java
+++ src/main/java/hu/peti/dev/farmassist/FarmAssist.java
@@ -18,24 +18,24 @@ import java.util.Arrays;
 import java.util.Map;
 
 public final class FarmAssist extends JavaPlugin implements Listener {
-    private final FarmAssist plugin;
+//    private final FarmAssist plugin;
 
-    public FarmAssist(FarmAssist plugin) {
-        this.plugin = plugin;
-    }
+//    public FarmAssist(FarmAssist plugin) {
+//        this.plugin = plugin;
+//    }
 
-    private final Map<Material,Material> cropMap = Map.of(
-            Material.WHEAT, Material.WHEAT_SEEDS,
+    private final Map<Material, Material> cropMap = Map.of(
+            Material.WHEAT, Material.WHEAT,
             Material.BEETROOTS, Material.BEETROOT_SEEDS,
             Material.CARROTS, Material.CARROT,
             Material.POTATOES, Material.POTATO,
-            Material.NETHER_WART_BLOCK, Material.NETHER_WART
+            Material.NETHER_WART, Material.NETHER_WART
     );
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this,this);
-        getServer().getPluginManager().registerEvents(new FarmAssist(this), this);
+        getServer().getPluginManager().registerEvents(this, this);
+//        getServer().getPluginManager().registerEvents(new FarmAssist(this), this);
     }
 
     @Override
@@ -44,18 +44,20 @@ public final class FarmAssist extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onBlockBreak(BlockBreakEvent event){
+    public void onBlockBreak(BlockBreakEvent event) {
         var block = event.getBlock();
+        System.out.println("Block destroyed: " + block);
         var player = event.getPlayer();
-        if(block.getBlockData() instanceof Ageable){
+        if (block.getBlockData() instanceof Ageable) {
+            System.out.println("Block was ageable");
             var blockMaterial = block.getBlockData().getMaterial();
-            //getLogger().info("Block break: " + event.getBlock());
-            if(!cropMap.containsKey(blockMaterial)) return;
+            if (!cropMap.containsKey(blockMaterial)) return;
             var seedMaterial = cropMap.get(blockMaterial);
-            if(event.getPlayer().getInventory().contains(seedMaterial)){
-                var seedStack =  Arrays.stream(event.getPlayer().getInventory().getContents()).filter(i -> i.getType().equals(seedMaterial)).findFirst().get();
-                if(seedStack.getAmount() > 1){
-                    seedStack.setAmount(seedStack.getAmount()-1);
+            System.out.println("Seed to plant: " + seedMaterial);
+            if (event.getPlayer().getInventory().contains(seedMaterial)) {
+                var seedStack = Arrays.stream(event.getPlayer().getInventory().getContents()).filter(i -> i.getType().equals(seedMaterial)).findFirst().get();
+                if (seedStack.getAmount() > 1) {
+                    seedStack.setAmount(seedStack.getAmount() - 1);
                 } else {
                     event.getPlayer().getInventory().remove(seedStack);
                 }
@@ -67,10 +69,17 @@ public final class FarmAssist extends JavaPlugin implements Listener {
                         int y = block.getY();
                         int z = block.getZ();
                         Block placedBlock = block.getWorld().getBlockAt(x, y, z);
-
-                        placedBlock.setType(Material.DIAMOND_BLOCK);
+                        System.out.println("Placing " + seedMaterial + " on " + x + " " + " " + y + " " + z + "...");
+//                        placedBlock.setType(seedMaterial);
+//                        placedBlock.setType(Material.WHEAT);
+                        switch (blockMaterial) {
+                            case WHEAT -> placedBlock.setType(Material.WHEAT_SEEDS); // Doesn't work
+                            case POTATOES -> placedBlock.setType(Material.POTATOES);
+                            case CARROTS -> placedBlock.setType(Material.CARROTS);
+                            case BEETROOTS -> placedBlock.setType(Material.BEETROOTS);
+                        }
                     }
-                }.runTask(this);
+                }.runTaskLater(this, 2);
             }
         }
     }
