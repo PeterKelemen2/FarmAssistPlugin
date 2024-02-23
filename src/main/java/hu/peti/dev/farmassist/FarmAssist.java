ce294df79eee831f3de9a71a7778cb9386d81ebc
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-23 10:36:25 +0100

    Fixed warnings

--- src/main/java/hu/peti/dev/farmassist/FarmAssist.java
+++ src/main/java/hu/peti/dev/farmassist/FarmAssist.java
@@ -1,32 +1,23 @@
 package hu.peti.dev.farmassist;
 
-import io.papermc.paper.event.block.BlockBreakBlockEvent;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Map;
 
 public final class FarmAssist extends JavaPlugin implements Listener {
-//    private final FarmAssist plugin;
-
-//    public FarmAssist(FarmAssist plugin) {
-//        this.plugin = plugin;
-//    }
 
     private final Map<Material, Material> cropMap = Map.of(
             Material.WHEAT, Material.WHEAT_SEEDS,
@@ -43,11 +34,12 @@ public final class FarmAssist extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(this, this);
 //        getServer().getPluginManager().registerEvents(new FarmAssist(this), this);
         PluginCommand command = getCommand("farmassist");
+        assert command != null;
         command.setExecutor(this);
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
         if (command.getName().equalsIgnoreCase("farmassist")) {
             if (args.length == 1) {
                 if ("toggle".startsWith(args[0].toLowerCase())) {
@@ -98,6 +90,7 @@ public final class FarmAssist extends JavaPlugin implements Listener {
                 if (seedStack != null && seedStack.getAmount() > 1) {
                     seedStack.setAmount(seedStack.getAmount() - 1);
                 } else {
+                    assert seedStack != null;
                     event.getPlayer().getInventory().remove(seedStack);
                 }
                 new BukkitRunnable() {
